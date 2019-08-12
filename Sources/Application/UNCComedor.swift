afad8c7136598c60387337d74b0382adb9d79969
Alejandro Ismael Silva <silva.alejandro.ismael@gmail.com>
2019-08-12 00:54:42 -0300

    Add guard when user is unparseable. Close #2

--- Sources/Application/UNCComedor.swift
+++ Sources/Application/UNCComedor.swift
@@ -34,6 +34,7 @@ class UNCComedor {
         case dataDecodingError
 
         case menuUnparseable
+        case userUnparseable
         case servingDateUnparseable
         case servingCountUnparseable
     }
@@ -173,9 +174,14 @@ class UNCComedor {
                 // Parse the data.
                 let preffix = "rows: [{c: ["
                 let suffix = "]}]}});"
-                let preffixIndex = dataString.range(of: preffix)!.upperBound
-                let suffixIndex = dataString.range(of: suffix)!.lowerBound
 
+                guard
+                    let preffixIndex = dataString.range(of: preffix)?.upperBound,
+                    let suffixIndex = dataString.range(of: suffix)?.lowerBound
+                else {
+                    callback(.failure(APIError.userUnparseable))
+                    return
+                }
                 let components = dataString[preffixIndex..<suffixIndex].components(separatedBy: "},{")
 
                 var _16 = components[16]
